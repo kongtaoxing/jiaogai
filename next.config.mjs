700a865d605d00668541e80a0e122b82b7f6e282
kongtaoxing <xyf8658@163.com>
2025-09-14 13:56:15 +0800

    fix: 8mb error

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // 增加静态文件大小限制
-  experimental: {
-    largePageDataBytes: 128 * 1024 * 1024, // 128MB
-  },
-  // 配置静态文件处理
-  async headers() {
-    return [
-      {
-        source: '/material/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
